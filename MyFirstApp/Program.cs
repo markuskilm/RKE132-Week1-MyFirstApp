de4f4fcb5a3fa8d92a9929732bd2ece5b0a51bda
Markus Kilm <markus.kilm@tktk.ee>
2024-02-04 23:04:34 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See on kommentaar minu koodile
+
+//rakendus küsib kasutajale sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+//string = sõne
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !");
